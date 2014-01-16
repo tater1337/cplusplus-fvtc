09841be11a98bd8c7ce64d324421ef3f7659709b
tater1337 <schuld9932.@fvtc.edu>
2014-01-16 11:21:45 -0600

    update to include
    int variables, formating of strings,  escape characters, control
    characters(/n /t)

--- output/output/Main.cpp
+++ output/output/Main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main()
 {
-	cout << "working";
+	int num = 28; // declare interger vaiable
+	cout << num; //does not need .ToString like c#
+	cout << " the number is :" << num << "." << endl; //instead of concatenating with + like in c#
+	cout << "working" << endl;
+	cout << "this is a lot of work to display a block of" << endl;
+	cout << "text\n";
+	// \ is an escape charater
+	// \n is a constrol character, does same as endl
+	// \t is a  tab character
+	cout << "this\tis\ttabbed\ttext" << endl; 
 
 	_getch();
 	return 1;
