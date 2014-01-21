804830e289c4abb4a7d29cf5f4415f34a6dec82e
tater1337 <schuld9932.@fvtc.edu>
2014-01-21 11:29:41 -0600

    output differetn variables

--- tuesday class week 4/variableDataTypes/variableDataTypes/main.cpp
+++ tuesday class week 4/variableDataTypes/variableDataTypes/main.cpp
@@ -1,16 +1,29 @@
 #include <conio.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int num = 5;
+	int num = -5;
+	int num2 = 7;
 	bool success = true;
+	double miles = 5.256;
+	string hello = "hello";
+	char c = 'y';
+
+	cout << "num = "<< num << endl;
+	cout << "success = " << success << endl;
+	cout << "miles = " << miles << endl;
+	cout << "hello = " << hello << endl; // string output needs #include <string>
+	cout << "c = " << c << endl;
+	cout << "num plus num2 equals " << num + num2 << endl;
 
 
-	cout << "testing";
-	
 	_getch();
+		
+	_getch();
+
 	return 1;
 }
