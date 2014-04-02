72e541dba1e3da598961b9902255fa2f076d1168
tater1337 <schuld9932.@fvtc.edu>
2014-04-02 15:24:39 -0500

    final assignment 15

--- Assignment15Structure/Assignment15Structure/main.cpp
+++ Assignment15Structure/Assignment15Structure/main.cpp
@@ -27,25 +27,29 @@ int main()
 		//
 
 		// 1 declare variables we need
-		// test data
-		
 		double grossPayTotal = 0;
+		string typeDesc[4] = {"Number","Name","Rate","Hours"};
 		Collection<Employee> Employees;
-				
 		Employee Unit;
-		Unit.number = 3462;
-		Unit.name = "Jobs";
-		Unit.rate = 4.62;
-		Unit.hours = 40;
+		
+		// test data ***************************************************************
+		//				
+		//Unit.number = 3462;
+		//Unit.name = "Jobs";
+		//Unit.rate = 4.62;
+		//Unit.hours = 40;
 
-		Employees.add(Unit);
+		//Employees.add(Unit);
 
-		Unit.number = 6793;
-		Unit.name = "Hooper";
-		Unit.rate = 5.83;
-		Unit.hours = 38;
+		//Unit.number = 6793;
+		//Unit.name = "Hooper";
+		//Unit.rate = 5.83;
+		//Unit.hours = 38;
 
-		Employees.add(Unit);
+		//Employees.add(Unit);
+		
+		//end test data *************************************************************
+
 		// 2 get variables from user
 		//Using the data type defined write a C++ program that interactively accepts the data into an array. 
 		//Be sure to use proper screen control to allow each employee to be entered on a clearly defined space. 
@@ -54,15 +58,32 @@ int main()
 		//Note: you will probably need to determine a maximum number of employees allowed, 
 		//and make sure your program does not go past that limit. You may (for 5 extra credit 
 		//points on this assignment) dynamically allocate memory as it is needed.
-		//loop until done
-
-		cin >> Unit.number;
-		cin >> Unit.name;
-		cin >> Unit.rate;
-		cin >> Unit.hours;
-
-		Employees.add(Unit);
 		
+		//loop until done
+		// there must be a way to make this into a for each loop.....I'm gonna try (dammit)
+		bool ExitDataEntry = false;
+		
+		while (ExitDataEntry == false)
+		{	
+			// i should put type checking loops arounf all of these, or add a funtion to helperbot to complete re-do cin so type errors are checked
+			cout << "Enter Employee " << typeDesc[0] << " or 0 to continue: "; //there should be a Q to quit and X to exit
+			cin >> Unit.number;  //won't let me use Unit.typeDesc[i]   :(
+			if (Unit.number != 0)
+			{
+			cout << "Enter Employee " << typeDesc[1] << ": ";
+			cin >> Unit.name;
+			cout << "Enter Employee " << typeDesc[2] << ": ";
+			cin >> Unit.rate;
+			cout << "Enter Employee " << typeDesc[3] << ": ";
+			cin >> Unit.hours;
+			cout << endl;
+			Employees.add(Unit);
+			}
+			else
+			{
+				ExitDataEntry = true;
+			}
+		}	
 		
 		// 3 do calculations
 		//loop through calculations
@@ -80,18 +101,25 @@ int main()
 		//Include the total grass pay for all employees at the end of the report.
 
 		//loop through outputs
+		cout << endl;
+		cout << endl;
+		cout << "Payroll Report" << endl;
+		cout << endl;
+		cout << typeDesc[0] << setw(9) << typeDesc[1] << setw(11) << "Gross pay" << endl;
+		cout << setprecision(2) << fixed;
 		for (int i = 0; i < Employees.Length(); i++)
 		{
 			Employee tempEmployee = Employees[i];
-			cout << tempEmployee.name << endl;
-			cout << grossPay[i] << endl;
+			cout << tempEmployee.number;
+			cout << setw(11) << tempEmployee.name;
+			cout << setw(11) << grossPay[i] << endl;
 
 		}
-		cout << grossPayTotal << endl;
 		cout << endl;
-		cout << "output" << endl;
+		cout << "Gross pay total" << setw(11) << grossPayTotal << endl;
+		cout << endl;
 
-		cout << "\nPress any key to loop or q to quit";
+		cout << "\nPress any key to loop or Q to quit";
 		char c = _getch();
 		if (c == 'q' || c == 'Q')
 		{
